41e1e58d45e6b1f8175ff9284d8f14ea3e471ed8
blerim2 <blerimballiu19@gmail.com>
2024-03-26 13:40:02 +0100

    trying

--- models/city.py
+++ models/city.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from os import getenv
+from sqlalchemy import Column, ForeignKey, String
 from models.base_model import BaseModel, Base
-# from models.state import State
-import models
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    else:
-        name = ''
-        state_id = ''
+    """
+    >>City inherits from BaseModel and Base (respect the order)
+    >>class attribute __tablename__ -
+            represents the table name, cities
+    >>class attribute name
+            represents a column containing a string (128 characters)
+            cant be null
+    >>class attribute state_id
+            represents a column containing a string (60 characters)
+            cant be null
+            is a foreign key to states.id
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
--- models/state.py
+++ models/state.py
@@ -3,27 +3,10 @@
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from os import getenv
-from models import storage
-from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        """ db ==>  means let's go for SQLAlchemy logic"""
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='all, delete', backref='state')
-    else:
-        name = ""
-
-    @property
-    def cities(self):
-        list_of_cities = []
-
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                list_of_cities.append(city)
-        return list_of_cities
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
