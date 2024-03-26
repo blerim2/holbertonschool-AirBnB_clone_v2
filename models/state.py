818c61e8d5aa04736a534bb12832abf01e0b978e
blerim2 <blerimballiu19@gmail.com>
2024-03-26 13:14:34 +0100

    trying

--- models/state.py
+++ models/state.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from os import getenv
-from models.city import City
 from models import storage
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
-    else:
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
         @property
         def cities(self):
             list_of_cities = []
