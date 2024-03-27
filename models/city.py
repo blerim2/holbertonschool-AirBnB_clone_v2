81ce5025265dd3315accd0418aa37eb504310300
blerim2 <blerimballiu19@gmail.com>
2024-03-27 10:39:56 +0100

    updating

--- models/city.py
+++ models/city.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""State class definitiont"""
-import models
+""" City Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship, backref
-from models.city import City
-from os import getenv
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
-class State(BaseModel, Base):
-    """This is the class for State."""
-    __tablename__ = "states"
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """ Returns the list of City"""
-            the_cities = []
-            for c in models.storage.all(City).values():
-                if c.state_id == self.id:
-                    the_cities.append(c)
-            return the_cities
+class City(BaseModel, Base):
+    """The city class, contains state ID and name."""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
