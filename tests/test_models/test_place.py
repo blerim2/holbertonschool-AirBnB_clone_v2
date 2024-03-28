926a239aba84e94bf4df54b661ab3ff7169a4519
MDrenova <mateodrenova@gmail.com>
2024-03-28 13:01:57 +0100

    Update: test'

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -58,12 +58,14 @@ class test_Place(test_basemodel):
     def test_latitude(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        if new.latitude != None:
+            self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        if new.latitude != None:
+            self.assertEqual(type(new.longitude), float)
 
     def test_amenity_ids(self):
         """ """
