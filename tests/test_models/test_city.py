59712670b8538f69b2019c82c95ae505b4d788bf
MDrenova <mateodrenova@gmail.com>
2024-03-28 13:07:55 +0100

    Update: test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -16,9 +16,11 @@ class test_City(test_basemodel):
     def test_state_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        if new.state_id != None:
+            self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        if new.name != None:
+            self.assertEqual(type(new.name), str)
