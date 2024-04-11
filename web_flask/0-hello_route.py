18c44c39ca89668de78eb3c2e9664b7df4eedfb4
blerim2 <blerimballiu19@gmail.com>
2024-04-11 10:26:48 +0200

    first commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+"""
+Module to initiate a flask app
+"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return("Hello HBNB!")
-
+def index():
+    """
+    Index route
+    """
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host='0.0.0.0', port=5000)
