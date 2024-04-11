c6f31a0a2b883732476a09bb095c5aaebc6175da
blerim2 <blerimballiu19@gmail.com>
2024-04-11 10:04:26 +0200

    first commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_1():
+    return("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb_2(text):
+    return("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def hbnb_3(text):
+    return("Python {}".format(text.replace("_", " ")))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=None)
