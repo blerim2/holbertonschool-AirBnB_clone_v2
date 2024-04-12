e873497f567e5f365ca52066928467f7982f333c
blerim2 <blerimballiu19@gmail.com>
2024-04-12 11:53:36 +0200

    method to handle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
-from flask import Flask, render_template
-from models import storage
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states_lis', strict_slashes=False)
-def states_list():
-    """ State list """
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+
+    States/cities are sorted by name.
+    """
     states = storage.all("State")
-    states = [state for state in states.values()]
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """ Method to handle """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
- 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
