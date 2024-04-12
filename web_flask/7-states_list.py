0b7de8bc202613c7a453bd93f56f1e06447e8b53
blerim2 <blerimballiu19@gmail.com>
2024-04-12 19:50:49 +0200

    dynamic routing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""import flask and models"""
-from flask import Flask, render_template
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
@@ -8,18 +11,22 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_states():
-    """variable show the list"""
-    state = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template("7-states_list.html", states=state)
-
-
 @app.teardown_appcontext
-def close_db(error):
-    """close"""
+def tear_db(exe):
+    """
+    Close storage on teardown
+    """
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    dynamic routing
+    """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
